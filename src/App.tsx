e7fb6ce2a907ecd6a1388d9a618a449b1a21d24e
gokuljs <gokuljs029@gmail.com>
2023-04-02 20:14:39 +0530

    portfolio website

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,51 @@ const Main = styled.div`
   width: 100vw;
   height: 100vh;
   background: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .portfolioHeader {
+    width: 90%;
+    height: 90%;
+    display: flex;
+    flex-basis: 1;
+    .details {
+      width: 50%;
+      color: white;
+      display: flex;
+      padding: 2rem 4rem;
+      align-items: center;
+      .info {
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        gap: 12px;
+        font-size: 2rem;
+        text-transform: uppercase;
+        font-family: "Varela", sans-serif;
+      }
+    }
+    .image {
+      width: 50%;
+    }
+  }
 `;
 
 function App() {
-  return <Main>hello world</Main>;
+  return (
+    <Main>
+      <div className="portfolioHeader">
+        <div className="details">
+          <div className="info">
+            <div>Hi</div>
+            <div>I'm Gokul JS</div>
+            <div>Founding Engineer At Aerotime</div>
+          </div>
+        </div>
+        <div className="image"></div>
+      </div>
+    </Main>
+  );
 }
 
 export default App;
